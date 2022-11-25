47aa3093b407ad2b635a973312b77126c961a655
Debbl <me@aiwan.run>
2022-11-25 22:42:15 +0800

    init

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("debbl.plugins")
+
+require("debbl.core.options")
+require("debbl.core.keymaps")
+
+-- plugins consfig
+require("debbl.plugins.configs.colorscheme")
+require("debbl.plugins.configs.comment")
+require("debbl.plugins.configs.nvim-tree")
+require("debbl.plugins.configs.lualine")
+require("debbl.plugins.configs.telescope")
+
